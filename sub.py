ac7c539d0f42a9cbecfc91b156b6d52232526d9f
shwetharmicrodegree <shwethar.microdegree@gmail.com>
2024-06-14 18:54:11 +0530

    2 programs

--- sub.py
+++ sub.py
@@ -3,7 +3,7 @@ num1 = 20
 num2 = 12
 
 # Adding two nos
-sum = num1 + num2
+sum = num1 - num2
 
 # printing values
-print("Sum of two numbers", num1, "and", num2 , "is", sum)
+print("Sub of", num1, "and", num2 , "is", sum)
